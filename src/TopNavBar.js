453575d35c08fb583922a00262149b679b920bf6
Wei-Yuan Shih <Frank@Wei-Yuans-MacBook-Pro.local>
2017-12-19 19:41:34 -0800

    add topnavbar

---
+++ src/TopNavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export class TopNavBar extends React.Component {
+    render() {
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+    }
+}
