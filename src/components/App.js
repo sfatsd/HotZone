006ce652de07235fabfe80f25683e4cf9e47d12d
Wei-Yuan Shih <Frank@Wei-Yuans-MacBook-Pro.local>
2017-12-19 20:04:03 -0800

    add logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../assets/images/logo.svg';
 import '../styles/App.css';
 import { TopNavBar } from "./TopNavBar";
 
@@ -8,9 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <TopNavBar/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
