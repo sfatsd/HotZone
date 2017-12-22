7750e6dcda74ac545d2f7b208012241dbffa4f25
Wei-Yuan Shih <Frank@Wei-Yuans-MacBook-Pro.local>
2017-12-21 20:35:42 -0800

    link shotchart and slider

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { ShotChart } from "./ShotChart";
-
+import { ShotChart } from './ShotChart';
+import { CountSlider } from './CountSlider';
 
 export class DataViewContainer extends React.Component {
+
+    state = {
+        minCount: 2,
+    }
+
+    onCountSliderChange = (value) => {
+        this.setState({ minCount: value });
+    }
+
     render() {
         return (
-            <ShotChart playerId = { this.props.playerId }/>
+            <div className="data-view">
+                <ShotChart playerId = { this.props.playerId } minCount = { this.state.minCount }/>
+                <div className="filters">
+                    <CountSlider
+                        minCount = { this.state.minCount }
+                        onCountSliderChange = { this.onCountSliderChange }
+                    />
+                </div>
+            </div>
         );
     }
 }
